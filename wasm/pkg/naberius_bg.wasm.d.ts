624bd1136cda44e8bbc3c845faf0aab381eee1bc
Your Name <you@example.com>
2023-02-04 16:32:32 +0000

    build

--- wasm/pkg/naberius_bg.wasm.d.ts
+++ wasm/pkg/naberius_bg.wasm.d.ts
@@ -3,7 +3,7 @@
 export const memory: WebAssembly.Memory;
 export function pack_right_unsafe(a: number, b: number, c: number, d: number): void;
 export function pack_left_unsafe(a: number, b: number, c: number, d: number): void;
-export function unpack_unsafe(a: number, b: number, c: number, d: number): void;
 export function xor_mod(a: number, b: number, c: number, d: number): void;
+export function unpack_unsafe(a: number, b: number, c: number, d: number): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_free(a: number, b: number): void;
